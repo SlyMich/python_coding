c35e79126199126756d9b99b882490e6cc9fe262
Sylvia <116837081+SylviaMichira@users.noreply.github.com>
2023-05-16 13:55:56 +0300

    Add files via upload
    calculating the sum and average of two operands using python language

---
+++ operands.py
@@ -0,0 +1,6 @@
+num1  = float (input("Enter first number="))
+num2 = float (input("Enter second number="))
+sum = num1+num2
+average = sum/2
+print("The sum is ",sum)
+print("The average is ",average)
