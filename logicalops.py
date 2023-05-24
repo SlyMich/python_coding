182412f954a20ab2a1172a37a3a494cb8339ee91
Sylvia <116837081+SylviaMichira@users.noreply.github.com>
2023-05-24 08:18:50 +0300

    Update logicalops.py
    use of logical and assignment operators in python

--- logicalops.py
+++ logicalops.py
@@ -1,6 +1,11 @@
+# Prompt the user to enter citizenship
 citizenship = input("Enter citizenship:")
+# Prompt the user to inputt Age in years
 age = int (input("Enter your age:"))
+# Test validity of citizenship and Age using logical and assignment operators
 if  age >= 18 and citizenship.lower() == "kenyan":
+    # Display this if the condition is met
     print ("You are eligible to vote?")
 else:
+    # Display this if condition is not met
     print("You can't vote.")
